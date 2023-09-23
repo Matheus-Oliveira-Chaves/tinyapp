5cf919df316684c94716ea702739c6232e33ed7a
Matheus-Oliveira-Chaves <Matheus.oliveira.chaves@hotmail.com>
2023-09-22 20:51:34 -0400

    Add login and usernme

--- express_server.js
+++ express_server.js
@@ -81,6 +81,12 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.post("/login", (req, res) => {
+  const { username } = req.body;
+  res.setHeader('Set-Cookie', `username=${username}`);
+  res.redirect("/urls");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
